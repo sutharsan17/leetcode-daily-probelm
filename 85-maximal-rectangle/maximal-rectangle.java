a41ee50c061a45fdfb6686eaf0fd27197f009f1a
sutharsan17 <136342958+sutharsan17@users.noreply.github.com>
2025-04-03 21:57:04 +0530

    Time: 15 ms (72.99%) | Memory: 47.4 MB (18.90%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int ans=0;
+        int[] arr=new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]=='0'){
+                    arr[j]=0;
+                }
+                else{
+                    arr[j]++;
+                }
+            }
+            ans=Math.max(ans,largest_histogram(arr));
+        }
+        return ans;
+    }
+    public static int largest_histogram(int[] arr){
+        Stack<Integer> st=new Stack<>();
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[i]<arr[st.peek()]){
+                int h=arr[st.pop()];
+                int r=i;
+                if(st.isEmpty()){
+                    ans=Math.max(ans,h*r);
+                }
+                else{
+                    int l=st.peek();
+                    ans=Math.max(ans,h*(r-l-1));
+                }
+            }
+            st.push(i);
+        }
+        int r=arr.length;
+        while(!st.isEmpty()){
+            int h=arr[st.pop()];
+            if(st.isEmpty()){
+                ans=Math.max(ans,h*r);
+            }
+            else{
+                int l=st.peek();
+                ans=Math.max(ans,h*(r-l-1));
+            }
+        }
+        return ans;
+    }
+}
