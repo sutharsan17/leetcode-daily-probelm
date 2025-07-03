9d3ebfe9a800e528f2a8fcc76a3a9d00b8b91d61
sutharsan17 <136342958+sutharsan17@users.noreply.github.com>
2025-07-03 12:58:39 +0530

    Time: 4 ms (23.78%) | Memory: 46.2 MB (22.49%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,30 +1,28 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int i =1;
-        int sum = 1;
-        while(i<n){
-            if(ratings[i]==ratings[i-1]){
-                i++;
-                sum++;
-                continue;
-            }
-            int peak = 1;
-            while(i<n && ratings[i]>ratings[i-1]){
-                peak++;
-                i++;
-                sum += peak;
-            }
-            int down = 1;
-            while(i<n && ratings[i]<ratings[i-1]){
-                sum += down;
-                i++;
-                down++;
-            }
-            if(down>peak){
-                sum += down - peak;
+        int[] candies = new int[n];
+
+        // Give 1 candy to each child initially
+        Arrays.fill(candies, 1);
+
+        // Left to right: handle increasing sequences
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
+
+        // Right to left: handle decreasing sequences
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Sum all candies
+        int sum = 0;
+        for (int c : candies) sum += c;
         return sum;
     }
 }
