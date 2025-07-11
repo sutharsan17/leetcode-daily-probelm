7a406f73a7f52775d74fd9fd9d8479f955abd7bb
sutharsan17 <136342958+sutharsan17@users.noreply.github.com>
2025-07-11 13:44:02 +0530

    Time: 39 ms (65.17%) | Memory: 53.4 MB (51.69%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,19 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(){
+      return "Hello World";
+    } 
+        
+    }
+
+
+
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
