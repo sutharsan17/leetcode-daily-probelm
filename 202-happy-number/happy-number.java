3a6717102b1217f8e6a47a7f22427d6780e0548a
sutharsan17 <136342958+sutharsan17@users.noreply.github.com>
2026-01-23 00:54:40 +0530

    Time: 1 ms (77.28%) | Memory: 42.9 MB (8.56%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> used=new HashSet<>();
+        while(true){
+            int sum=0;
+            while(n!=0){
+                sum+=Math.pow(n%10,2.0);
+                n=n/10;
+            }
+
+            if(sum==1) return true;
+            n=sum;
+            if(used.contains(n))return false;
+            used.add(n);
+        }
+        
+    }
+}
